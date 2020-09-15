696677cc0be68ab3ce706f0aee2459ba57e5a3b9
ksrinivasulu-vb <ksrinivasulu.vb@gmail.com>
2020-09-15 18:40:00 +0530

    comit by srini

---
+++ consoleproject/src/consoleproject/testconsole.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package consoleproject;
+
+/**
+ * @author varsha
+ *
+ */
+public class testconsole {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
